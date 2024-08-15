44117614c5325cf6f9efaaa519800573a439a987
arungane944 <134732953+arungane944@users.noreply.github.com>
2024-08-15 16:49:33 +0530

    control flow

---
+++ Python/4. Controlflow/Nested_ifelse.py
@@ -0,0 +1,8 @@
+value = int(input())
+
+if value > 0:
+    print("positive number")
+elif value == 0:
+    print("value is zero")
+else:
+    print("negative number")
---
+++ Python/4. Controlflow/ifelse.py
@@ -0,0 +1,7 @@
+age = int(input())
+
+if(age > 18):
+    print ("you are eligible")
+    print("please drive slow")
+else:
+    print("wait till turn 18")
