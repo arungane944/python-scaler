1e1b3f14b00b1c961ce1fd656ff68f1f7d097a1c
arungane944 <134732953+arungane944@users.noreply.github.com>
2024-08-15 14:00:24 +0530

    print function

---
+++ Python/2. Print function/1. printfunction.py
@@ -0,0 +1,6 @@
+print("hello", "world", 'india' , 123 , False, 343.98)
+print("hello", "world", 'india' , 123 , False, 343.98, sep="->")
+print("hello", "world", 'india' , 123 , False, 343.98, sep="\n")
+
+print("Arun", 24, end=" ")
+print("Fiserv", "Devops Enginer")
