377a87b1f282cc535e21ae2c3d5b6630c3811924
arungane944 <134732953+arungane944@users.noreply.github.com>
2024-08-15 15:43:29 +0530

    opeartors

---
+++ Python/3. Operators/operators.py
@@ -0,0 +1,8 @@
+a = 52
+b = 52
+
+print(id(a) , (id(b)), sep=',')
+
+# The is operator in Python is used to compare the identity of two objects. It checks whether two variables point to the same object in memory, not just if their values are the same.
+print(a is b)
+
