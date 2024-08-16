01e07bd26e1e7dc3b8fad28916d58cb528a44d81
arungane944 <134732953+arungane944@users.noreply.github.com>
2024-08-16 11:47:09 +0000

    revision 1

--- Python/5. loops/example.py
+++ Python/5. loops/example.py
@@ -1,6 +1,11 @@
-for x in range(10):
-       if x == 6:
-           print(x,end="")
-print(x)
+#Write a for loop that calculates the sum of all numbers from 1 to 100.
+sum = 0
+for i in range(1,101):
+    sum+=i
+print(sum)
 
 
+# Create a nested for loop that prints a multiplication table from 1 to 10.
+
+for i in range(1,11):
+    print(i, "x 10 = ", i*10)
